95f1206829fa12e29110dc0ea4917f52678fde38
Chris Fu <cdf6gc@uw.edu>
2016-02-29 11:41:27 -0800

    Addressed issues #4-6

--- HDSAviz/plotting.py
+++ HDSAviz/plotting.py
@@ -52,20 +52,25 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
     #                  engine='python')
     # df = tdf
     df = dataframe
-    maxval = 0
+
     # Remove rows which have values less than cutoff values
     df = df[df['S1'] > minvalues]
     df = df[df['ST'] > minvalues]
     df = df.dropna()
 
-    # Create dictionary of bar colors, assume up to 3 stats are plotted
-    # per parameter.
+    # Only keep top values indicated by variable top
     df = df.sort_values('ST', ascending=False)
     df = df.head(top)
     df = df.reset_index(drop=True)
+
+    # Create arrays of colors and order labels for plotting
     colors = ["#0d3362", "#c64737"]
     s1color = np.array(["#c64737"]*df.S1.size)
     sTcolor = np.array(["#0d3362"]*df.ST.size)
+    firstorder = np.array(["1st (S1)"]*df.S1.size)
+    totalorder = np.array(["Total (ST)"]*df.S1.size)
+
+    # Create Dictionary of colors
     stat_color = OrderedDict()
     for i in range(0, 2):
         stat_color[i] = colors[i]
@@ -80,8 +85,9 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
     # Determine wedge size based off number of parameters
     big_angle = 2.0 * np.pi / (len(df)+1)
     # Determine division of wedges for plotting bars based on # stats plotted
+    # for stacked or unstacked bars
     if stacked is False:
-        small_angle = big_angle / (4)
+        small_angle = big_angle / (5)
     else:
         small_angle = big_angle / (3)
     # pdb.set_trace()
@@ -93,55 +99,59 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
     #             ResizeTool(), PanTool(), PolySelectTool(),
     #             WheelZoomTool(), HoverTool(), BoxSelectTool()]
     plottools = "hover, wheel_zoom, save, reset, resize"
-
+    # Initialize figure with tools, coloring, etc.
     p = figure(plot_width=width, plot_height=height, title="",
                x_axis_type=None, y_axis_type=None,
                x_range=(-420, 420), y_range=(-420, 420),
                min_border=0, outline_line_color="black",
                background_fill_color="#f0e1d2", border_fill_color="#f0e1d2",
                tools=plottools)
+    # Specify labels for hover tool
     hover = p.select(dict(type=HoverTool))
-    hover.tooltips = [("Parameter", "@Param"), ("Sensitivity", "@Sens"),
-                      ("Confidence", "@Conf")]
+    hover.tooltips = [("Order", "@Order"), ("Parameter", "@Param"),
+                      ("Sensitivity", "@Sens"), ("Confidence", "@Conf")]
     hover.point_policy = "follow_mouse"
 
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_color = None
 
     # annular wedges divided into smaller sections for bars
+    # Angles for axial line placement
+    num_lines = np.arange(0, len(df)+1, 1)
+    line_angles = np.pi/2 - big_angle/2 - num_lines*big_angle
+
+    # Angles for data placement
     angles = np.pi/2 - big_angle/2 - df.index.to_series()*big_angle
+
     # circular axes and labels
     labels = np.power(10.0, np.arange(0, -3, -1))
-    # extra_labels = np.power(10.0, np.arange(-0.30103, -4.30103, -1))
-    # Re-size if no values are above 0.1
-    # if maxval < 0.1:
-    #     labels = np.delete(labels, 0)
-    # Size of radii of each label
-    # pdb.set_trace()
-    # extra_radii = (((np.log10(extra_labels / labels[0])) + labels.size) *
-    #                (outer_radius - inner_radius) / labels.size +inner_radius)
+
+    # Set max radial line to correspong to 1.1* maximum value + error
+    maxval = max(df.ST)
+    maxval_index = df.ST.argmax()
+    maxval_conf = df.ST_conf[maxval_index]
     labels = np.append(labels, 0.0)
+    labels[0] = 1.1*(maxval+maxval_conf)
+
+    # Determine if radial axis are log or linearly scaled
     if lgaxis is True:
             radii = (((np.log10(labels / labels[0])) +
                      labels.size) * (outer_radius - inner_radius) /
                      labels.size + inner_radius)
             radii[-1] = inner_radius
     else:
-        labels = np.delete(labels, -1)
-        radii = (outer_radius - inner_radius)*labels + inner_radius
-
-    # Add zero label to labels and radii
-
-    # extra_radii = np.append(extra_radii, radii)
+        labels = np.delete(labels, -2)
+        radii = (outer_radius - inner_radius)*labels/labels[0] + inner_radius
 
     # Adding axis lines and labels
     p.circle(0, 0, radius=radii, fill_color=None, line_color="white")
     p.text(0, radii[:], [str(r) for r in labels[:]],
            text_font_size="8pt", text_align="center", text_baseline="middle")
 
-    # Plot the values of each stat for each parameter.
-    # radius of stat is the value of a statistic converted to the radial
-    # value.
+    # Convert sensitivity values to the plotted values
+    # Same conversion as for the labels above
+    # Also calculate the angle to which the bars are placed
+    # Add values to the dataframe for future reference
     Cols = np.array(['S1', 'ST'])
     for statistic in range(0, 2):
         if lgaxis is True:
@@ -150,7 +160,7 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
                               labels.size + inner_radius)
         else:
             radius_of_stat = ((outer_radius - inner_radius) *
-                              df[Cols[statistic]] + inner_radius)
+                              df[Cols[statistic]]/labels[0] + inner_radius)
         if stacked is False:
             startA = -big_angle + angles + (2*statistic + 1)*small_angle
             stopA = -big_angle + angles + (2*statistic + 2)*small_angle
@@ -165,6 +175,7 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
                                                       index=df.index)
         inner_rad = np.ones_like(angles)*inner_radius
 
+    # Store plotted values into dictionary to be add glyphs
     pdata = pd.DataFrame({
                          'x': np.append(np.zeros_like(inner_rad),
                                         np.zeros_like(inner_rad)),
@@ -191,11 +202,13 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
                          'Conf': pd.Series.append(df.ST_conf,
                                                         df.S1_conf
                                                   ).reset_index(drop=True),
+                         'Order': np.append(totalorder, firstorder),
                          'Sens': pd.Series.append(df.ST,
                                                   df.S1).reset_index(drop=True)
                          })
 
     pdata_s = ColumnDataSource(pdata)
+    # Specify that the plotted bars are the only thing to activate hovertool
     gh = p.annular_wedge(x='x', y='y', inner_radius='ymin',
                          outer_radius='ymax',
                          start_angle='starts',
@@ -204,15 +217,13 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
                          source=pdata_s
                          )
     hover.renderers = [gh]
-    # p.annular_wedge(0, 0, inner_radius, df.STradial,
-    #                 -big_angle + angles + (2*1 + 1)*small_angle,
-    #                 -big_angle + angles + (2*1 + 2)*small_angle,
-    #                 color=stat_color[1])
-    # radial axes
 
-    dictdata = df.set_index('Parameter').to_dict()
+    # dictdata = df.set_index('Parameter').to_dict()
+
+    # plot lines to separate parameters
     p.annular_wedge(0, 0, inner_radius-10, outer_radius+10,
-                    -big_angle+angles, -big_angle+angles, color="black")
+                    -big_angle+line_angles, -big_angle+line_angles,
+                    color="black")
 
     # Placement of parameter labels
     xr = radii[0]*np.cos(np.array(-big_angle/2 + angles))
@@ -220,6 +231,7 @@ def make_plot(dataframe,  top=100, minvalues=0.01, stacked=True, lgaxis=True):
 
     label_angle = np.array(-big_angle/2+angles)
     label_angle[label_angle < -np.pi/2] += np.pi
+
     # Placing Labels and Legend
     p.text(xr, yr, df.Parameter, angle=label_angle,
            text_font_size="9pt", text_align="center", text_baseline="middle")
