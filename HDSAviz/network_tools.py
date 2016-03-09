a295de79a50d45917de55552604f15df2f2fdcd2
houghb <houghb@uw.edu>
2016-03-08 16:51:07 -0800

    fixed another typo in network_tools

--- HDSAviz/network_tools.py
+++ HDSAviz/network_tools.py
@@ -18,7 +18,7 @@ try:
     from graph_tool.all import *
 except ImportError:
     print ('graph-tool package is not installed!\n You will not be able to '
-           'use functions from `network_tools`)
+           'use functions from `network_tools`')
 
 
 def build_graph(df_list, sens='ST', top=410, min_sens=0.01,
