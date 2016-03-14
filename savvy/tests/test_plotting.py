81574e2265523c7461bfce2753c5c5d6ab9f903b
Chris Fu <cdf6gc@uw.edu>
2016-03-13 18:21:05 -0700

    updated to use sample data files

--- savvy/tests/test_plotting.py
+++ savvy/tests/test_plotting.py
@@ -2,15 +2,26 @@
 
 import unittest
 
+import pandas as pd
+import os
+import pdb
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
 from ..plotting import make_plot
-from ..data_processing import get_sa_data
+
+sample_files_path = os.getcwd().split('savvy')[0] + 'savvy/sample_data_files/'
+comparisons = pickle.load(open(sample_files_path+'unittest_comparisons.pkl',
+                               'rb'))
+df = comparisons[0]['sample-output1'][0]
 
 
 class TestPlots(unittest.TestCase):
 
     def test_incorrect_dataframe(self):
         """Check dataframe has incorrect headers"""
-        sa_dict = get_sa_data()
         with self.assertRaises(Exception) as context:
             make_plot()
         self.assertTrue('Dataframe not formatted correctly' in
@@ -18,56 +29,49 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_dataframe(self):
         """Check dataframe has correct headers"""
-        sa_dict = get_sa_data()
-        make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        make_plot(dataframe=df, highlight=[],
                   top=100, minvalues=0.01, stacked=True, lgaxis=True,
                   errorbar=True, showS1=True, showST=True)
         self.assert_(True)
 
     def test_correct_height(self):
         """Check radial plot height is 800"""
-        sa_dict = get_sa_data()
-        result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        result = make_plot(dataframe=df, highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(result.properties_with_values()['plot_height'], 800)
 
     def test_correct_width(self):
         """Check radial plot width is 800"""
-        sa_dict = get_sa_data()
-        result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        result = make_plot(dataframe=df, highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(result.properties_with_values()['plot_width'], 800)
 
     def test_correct_number_of_glyphs_radial(self):
         """Check number of glyph renders created (radial plot)"""
-        sa_dict = get_sa_data()
-        result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        result = make_plot(dataframe=df, highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(len(result.properties_with_values()['renderers']), 12)
 
     def test_correct_number_of_tools_radial(self):
         """Check number of tools created (radial plot)"""
-        sa_dict = get_sa_data()
-        result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        result = make_plot(dataframe=df, highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(len(result.properties_with_values()['tools']), 5)
 
     def test_correct_number_of_glyphs_bar(self):
         """Check number of glyph renders created (bar chart)"""
-        sa_dict = get_sa_data()
-        result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        result = make_plot(dataframe=df, highlight=[],
                            top=5, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(len(result.properties_with_values()['renderers']), 15)
 
     def test_correct_number_of_tools_bar(self):
         """Check number of tools created (bar chart)"""
-        sa_dict = get_sa_data()
-        result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
+        result = make_plot(dataframe=df, highlight=[],
                            top=5, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(len(result.properties_with_values()['tools']), 7)
