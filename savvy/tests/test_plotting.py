49c08c16bc6867686b7eb9d0c348bb3d60399b92
houghb <houghb@uw.edu>
2016-03-13 17:55:48 -0700

    renamed the unittest file for plotting.py and added relative paths so it works from the tests/ directory

--- savvy/tests/test_plotting.py
+++ savvy/tests/test_plotting.py
@@ -1,16 +1,16 @@
 '''Unit tests for make_plot'''
 
 import unittest
-from plotting import make_plot
-import pandas as pd
-import data_processing as dp
+
+from ..plotting import make_plot
+from ..data_processing import get_sa_data
 
 
 class TestPlots(unittest.TestCase):
 
     def test_incorrect_dataframe(self):
         """Check dataframe has incorrect headers"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         with self.assertRaises(Exception) as context:
             make_plot()
         self.assertTrue('Dataframe not formatted correctly' in
@@ -18,7 +18,7 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_dataframe(self):
         """Check dataframe has correct headers"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                   top=100, minvalues=0.01, stacked=True, lgaxis=True,
                   errorbar=True, showS1=True, showST=True)
@@ -26,7 +26,7 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_height(self):
         """Check radial plot height is 800"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
@@ -34,7 +34,7 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_width(self):
         """Check radial plot width is 800"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
@@ -42,7 +42,7 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_number_of_glyphs_radial(self):
         """Check number of glyph renders created (radial plot)"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
@@ -50,7 +50,7 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_number_of_tools_radial(self):
         """Check number of tools created (radial plot)"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                            top=100, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
@@ -58,7 +58,7 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_number_of_glyphs_bar(self):
         """Check number of glyph renders created (bar chart)"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                            top=5, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
@@ -66,10 +66,11 @@ class TestPlots(unittest.TestCase):
 
     def test_correct_number_of_tools_bar(self):
         """Check number of tools created (bar chart)"""
-        sa_dict = dp.get_sa_data()
+        sa_dict = get_sa_data()
         result = make_plot(dataframe=sa_dict['totaltars'][0], highlight=[],
                            top=5, minvalues=0.01, stacked=True, lgaxis=True,
                            errorbar=True, showS1=True, showST=True)
         self.assertEqual(len(result.properties_with_values()['tools']), 7)
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
